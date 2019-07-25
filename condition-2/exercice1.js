800c64724ecbbb775637697f5fbc916c5500f9cc
parvedyn <nparvedysimplon@gmail.com>
2019-07-25 15:08:39 +0200

    ajout condition exercice 1

---
+++ condition-2/exercice1.js
@@ -0,0 +1,9 @@
+var name = prompt('Bonjour !');
+if (name === "bonjour"){
+    alert('Bonjour à toi !');
+} else if (name === "tu vas bien ?"){
+    alert('Bien, et toi ?');
+} else{
+    alert('Désolé, je n\'ai pas compris...');
+}
+
