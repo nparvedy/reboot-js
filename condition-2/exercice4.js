7bbb6a74cfed5925d8418a50693bee9a5b67127a
NParvedy <nicolasparvedy@gmail.com>
2019-07-27 01:49:25 +0200

    Exercice4.js fini

--- condition-2/exercice4.js
+++ condition-2/exercice4.js
@@ -1,25 +1,27 @@
 var nombreAleatoire = Math.round(Math.random() * 100);
 
 var nombreUtilisateur = prompt('Quel est votre chiffre');
-console.log(nombreUtilisateur);
 
 for(var i = 0; i < 10; i++){
     
-    var nombreEssai = i;
-    if(nombreAleatoire == nombreUtilisateur){
-        alert('Bravo tu as le chiffre ' + nombreAleatoire + ' en ' + nombreEssai + ' !');
+    var nombreEssai = i + 1;
+    if(nombreAleatoire == nombreUtilisateur && nombreEssai === 1){
+        alert('Bravo tu as trouvé du premier coup !');
         break;
-    }else if(nombreAleatoire !== nombreUtilisateur){
-        nombreEssai = i + 1;
-        var nombreUtilisateur = prompt('Essaye encore ! Tu es actuellement sur ' + nombreEssai + '/10.');
-        console.log(nombreEssai);
-        if(nombreEssai == 9){
-            nombreEssai = nombreEssai + 1;
-            alert('Perdu ! Tu n\'as plus d\'essai ! Tu es actuellement sur ' + nombreEssai + '/10.');
-            break;
-        }
+    }else if(nombreAleatoire == nombreUtilisateur){
+        alert('Bravo tu as le chiffre ' + nombreAleatoire + ' en ' + nombreEssai + ' essais !');
+        break;
+    }else if(nombreEssai == 10){
+        alert('Perdu ! Tu n\'as plus d\'essai ! Tu es actuellement sur ' + nombreEssai + '/10.');
+        break;
+    }else if (nombreAleatoire < nombreUtilisateur){
+        var nombreUtilisateur = prompt('plus petit ' + nombreEssai + '/10.');
+    }else if (nombreAleatoire > nombreUtilisateur){
+        var nombreUtilisateur = prompt('plus grand ' + nombreEssai + '/10.');
     }else{
-        alert('Une erreur est survenue !');
+        alert('Une erreur est survenue');
         break;
     }
 }
+
+
