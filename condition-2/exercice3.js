71bef2b58c6860617e3238caa372cbd3971ec257
parvedyn <nparvedysimplon@gmail.com>
2019-07-26 13:21:13 +0200

    modif exercice3.js

--- condition-2/exercice3.js
+++ condition-2/exercice3.js
@@ -1,14 +1,14 @@
-var nomDeLaVariable = prompt('Bonjour tu vas bien ?');
+var nomDeLaVariable = prompt('Bonjour tu vas bien ? (oui ou non)');
 if (nomDeLaVariable === 'oui'){
     nomDeLaVariable = prompt('Tu préfères le rouge ou le bleu ?');
-    if (nomDeLaVariable === "rouge"){
+    if (nomDeLaVariable === "bleu"){
         nomDeLaVariable = prompt('Comme la mer ?')
         if (nomDeLaVariable === "oui"){
             alert('Ah! Super j\'adore la mer');
         }else{
             alert('Oh ! Moi qui préfère la mer !');
         }
-    }else if (nomDeLaVariable === "bleu"){
+    }else if (nomDeLaVariable === "rouge"){
         nomDeLaVariable = prompt('Comme le sang ?');
         if (nomDeLaVariable === 'oui'){
             alert('Ola la il fait tout rouge !');
@@ -19,7 +19,7 @@ if (nomDeLaVariable === 'oui'){
         alert('Une erreur s\'est produit');
     }
 }else if (nomDeLaVariable === 'non'){
-    nomDeLaVariable = prompt('Tu préfères les chiens ou chat ?');
+    nomDeLaVariable = prompt('Tu préfères les chiens ou chats ?');
     if (nomDeLaVariable === 'chiens'){
         alert('waouf');
     } else if (nomDeLaVariable === 'chats'){
